fcc80282d21534ff243cc96b04dcd2049048a9f1
simplyObare <143612431+simplyObare@users.noreply.github.com>
2024-04-10 00:59:59 +0300

    update in Form component

--- myapp/src/Components/Form.jsx
+++ myapp/src/Components/Form.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 
 export default function Form() {
-  const [name, setName] = useState('')
+  const [name, setName] = useState({ firstName: '', secondName: '' })
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(name)
+  }
 
   return (
     <div>
       <form>
         <input
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
           type="text"
-          value={name}
+          value={name.firstName}
         />
+        <input
+          onChange={(e) => setName({ ...name, secondName: e.target.value })}
+          type="text"
+          value={name.secondName}
+        />
+        <button onClick={(e) => handleSubmit(e)}>ADD</button>
       </form>
     </div>
   )
