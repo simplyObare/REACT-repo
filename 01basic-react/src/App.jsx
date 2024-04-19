c9079ccb96b8dd3a56e0ecfc8a23c57f9b10300b
simplyObare <143612431+simplyObare@users.noreply.github.com>
2024-04-19 17:59:01 +0300

    counter app

--- 01basic-react/src/App.jsx
+++ 01basic-react/src/App.jsx
@@ -1,22 +1,16 @@
 import { useState } from 'react'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React App</h1>
+      <h2>Counter value: {count} </h2>
+      <button onClick={() => setCount(count + 1)}>Add value</button>{' '}
+      <button onClick={() => setCount(count - 1)}>Remove value</button>{' '}
+      <button onClick={() => setCount(0)}>Reset value</button>
     </>
   )
 }
