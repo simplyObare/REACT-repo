8c76dc68f8ae72a4bc9014f93ca3263cff9bae39
simplyObare <josephatowiny@gmail.com>
2024-05-20 16:18:32 +0300

    useLayoutEffect

--- Learn-React-Hooks/useReducer/src/App.jsx
+++ Learn-React-Hooks/useReducer/src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dispatch, count] = useReducer((count) => count + 1, 0)
 
   return (
     <>
       <h1>{count}</h1>
-      <button
-        onClick={() => setCount((prev) => prev + 1)}
-        style={{ marginRight: '10px' }}
-      >
+      <button onClick={dispatch} style={{ marginRight: '10px' }}>
         Increase
       </button>
-      <button onClick={() => setCount((prev) => prev - 1)}>Decrease</button>
+      <button>Decrease</button>
     </>
   )
 }
