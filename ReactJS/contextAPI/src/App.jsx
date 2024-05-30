7a3af84e92c261de67ba84bc0cfd19929c815a87
simplyObare <josephatowiny@gmail.com>
2024-05-30 21:04:40 +0300

    contextAPI
    deleted the redux tutorial

---
+++ ReactJS/contextAPI/src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import Cart from './components/Cart'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </>
+  )
+}
+
+export default App
